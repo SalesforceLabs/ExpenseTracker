669cec8750d73051c9b94596f6a91260a5c334fb
Diwas Poudel <diwas.poudel@salesforce.com>
2020-04-11 15:07:12 -0700

    data model and sample charts

---
+++ force-app/main/default/lwc/expenseLineChart/expenseLineChart.js
@@ -0,0 +1,109 @@
+import { LightningElement } from 'lwc';
+
+import { loadScript } from 'lightning/platformResourceLoader';
+import chartjs from '@salesforce/resourceUrl/chart';
+
+const randomScalingFactor = () => {
+    return Math.round(Math.random(0, 100));
+};
+
+const chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+
+export default class ExpenseLineChart extends LightningElement {
+    error;
+    chart;
+    chartjsInitialized = false;
+
+    config = {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: chartColors.red,
+                borderColor: chartColors.red,
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor()
+                ],
+                fill: false,
+            }, {
+                label: 'My Second dataset',
+                fill: false,
+                backgroundColor: chartColors.blue,
+                borderColor: chartColors.blue,
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor()
+                ],
+            }]
+        },
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                x: {
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Month'
+                    }
+                },
+                y: {
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value'
+                    }
+                }
+            }
+        }
+    };
+
+    renderedCallback() {
+        if (this.chartjsInitialized) {
+            return;
+        }
+        this.chartjsInitialized = true;
+
+        loadScript(this, chartjs)
+            .then(() => {
+                const canvas = document.createElement('canvas');
+                this.template.querySelector('div.chart').appendChild(canvas);
+                const ctx = canvas.getContext('2d');
+                this.chart = new window.Chart(ctx, this.config);
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
+}
